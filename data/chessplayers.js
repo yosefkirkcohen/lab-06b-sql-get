162590160828601a6a279b3de3194cf343f0ba58
yosefkirkcohen <ycohen45@yahoo.com>
2021-09-29 15:45:08 -0700

    initial commit

---
+++ data/chessplayers.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    name: 'Magnus Carlsen',
+    rating: 2847,
+    worldChampion: true,
+    country: 'Norway', 
+    id: 1
+  },
+  {
+    name: 'Fabiano Caruana',
+    rating: 2820,
+    worldChampion: false,
+    country: 'USA', 
+    id: 2
+  },
+  {
+    name: 'Ding Liren',
+    rating: 2791,
+    worldChampion: false,
+    country: 'China', 
+    id: 3
+  },
+  {
+    name: 'Ian Nepomniatchi',
+    rating: 2789,
+    worldChampion: false,
+    country: 'Russia', 
+    id: 4
+  },
+  {
+    name: 'Levon Aronian',
+    rating: 2781,
+    worldChampion: false,
+    country: 'Armenia', 
+    id: 5
+  }
+];
