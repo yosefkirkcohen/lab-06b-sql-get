a49ab964689d4802ed028ddf2ca8126d7b71a211
yosefkirkcohen <ycohen45@yahoo.com>
2021-10-04 15:00:29 -0700

    create categories table

---
+++ data/categories.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    category: 'grandmaster'
+  },
+  {
+    category: 'international_master'
+  },
+  {
+    category: 'fide_master'
+  },
+  {
+    category: 'national_master'
+  },
+];
